3b50584396bef8e7e3bb0fc211a2a636c952575e
Yulia <yuliafamusova@gmail.com>
2023-06-13 14:15:08 +0300

    2.2

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-let m = 20;
+const fs = require('fs')
+const path = require('path')
+const axios = require('axios')
+const http = require('http')
 
-simpleNumbers:
-    for (let i = 2; i <= m; i++) {
+// ;(async () => {
+//     const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+//     fs.writeFile(path.resolve(__dirname, '1.json'), JSON.stringify(data), 'utf-8', (err) => {
+//         if (err) {
+//             throw err;
+//         }
+//         console.log('Done!');
+//     })
+// }) ()
 
-        for (let n = 2; n < i; n++) {
-            if (i % n == 0) continue simpleNumbers
-        }
-
-        console.log(i);
-
+http
+.createServer((request, response) => {
+    response.setHeader("Content-type", "text/html; charset=utf-8;")
+    if (request.url === '/users') {
+        response.write('Список пользователей')
+    } else if (request.url === '/posts'){
+        response.write('Список постов')
     }
+    response.end()
+})
+.listen(3000)
+
+
